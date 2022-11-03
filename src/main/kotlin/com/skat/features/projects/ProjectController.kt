6cf94918291882c7bcde85a99570f9e841fae7c7
4118BarbolinS.P <skat.apps@gmail.com>
2022-11-04 00:35:29 +0300

    Добавление id проекта в студию при добавление элемента в таблицу projects

--- src/main/kotlin/com/skat/features/projects/ProjectController.kt
+++ src/main/kotlin/com/skat/features/projects/ProjectController.kt
@@ -1,7 +1,13 @@
 package com.skat.features.projects
 
+import com.skat.database.event_for_project.EventDTO
+import com.skat.database.event_for_project.Events
 import com.skat.database.projects.ProjectDTO
 import com.skat.database.projects.Projects
+import com.skat.database.studious.Studious
+import com.skat.features.projects.tasks.ListResponceModel
+import com.skat.features.projects.tasks.ListStringReceiveModel
+import com.skat.features.studious.StudioUpdateReceiveModel
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
@@ -16,7 +22,6 @@ object ProjectController {
 
         Projects.insert(
 
-
             ProjectDTO(
                 id = id,
                 adminId = receiveModel.adminId,
@@ -24,6 +29,13 @@ object ProjectController {
                 description = receiveModel.description,
             )
         )
+
+        val projects = Studious.fetchStudio(receiveModel.studio)!!.projects.toMutableList()
+        projects.add(id)
+        Studious.updateStudio(StudioUpdateReceiveModel(
+            name = receiveModel.studio,
+            project = projects.toTypedArray()
+        ))
 
         val project = Projects.fetchProject(id)
         if (project != null) { call.respond(HttpStatusCode.OK, id)}
@@ -47,19 +59,27 @@ object ProjectController {
         }
     }
 
-    suspend fun fetchProject(call: ApplicationCall) {
-        val id = call.parameters["id"]
+    private fun getProjectFromDB(indecies: List<String>): ListResponceModel<ProjectDTO> {
 
-        if (id == null) {
-            call.respond(HttpStatusCode.BadRequest, "there isn't parametr get")
-        } else {
-            val project = Projects.fetchProject(id)
+        val responseProject = mutableListOf<ProjectDTO>()
+        val failElementIndex = mutableListOf<String>()
 
-            if (project == null) {
-                call.respond(HttpStatusCode.BadRequest, "not found project")
+        for (index in indecies) {
+            val tempEvent = Projects.fetchProject(index)
+
+            if (tempEvent != null) {
+                responseProject.add(tempEvent)
             } else {
-                call.respond(HttpStatusCode.OK, project)
+                failElementIndex.add(index)
             }
         }
+
+        return ListResponceModel(responseProject, failElementIndex)
+    }
+
+    suspend fun getProjects(call: ApplicationCall) {
+        val receive = call.receive(ListStringReceiveModel::class)
+        val result = getProjectFromDB(receive.ides)
+        call.respond(HttpStatusCode.OK, result)
     }
 }
